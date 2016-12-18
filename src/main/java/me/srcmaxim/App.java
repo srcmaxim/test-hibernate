3c95af2f92b742b1f6b5c61c838aea97b6b529ba
srcmaxim <srcmaxim@gmail.com>
2016-12-18 16:04:51 +0200

    Add example of Restrictions in Criteria API

--- src/main/java/me/srcmaxim/App.java
+++ src/main/java/me/srcmaxim/App.java
@@ -9,11 +9,16 @@ import static me.srcmaxim.util.HibernateUtil.*;
 public class App {
 
     public static void main(String[] args) {
-        IntStream.rangeClosed(1, 5).mapToObj(i -> new User("User " + i))
+        IntStream.rangeClosed(1, 20).mapToObj(i -> new User("User " + i))
                 .forEach(user -> openInASession(session -> session.save(user)));
 
         openInASession(session -> {
-            session.createCriteria(User.class).add(Restrictions.eq("username", "User 5"))
+            session.createCriteria(User.class)
+                    .add(Restrictions.or(
+                            Restrictions.and(
+                                    Restrictions.like("username", "User 1_"),
+                                    Restrictions.ge("userId", 15)),
+                            Restrictions.lt("userId", 6)))
                     .list().stream().map(Object::toString).forEach(System.out::println);
         });
     }
