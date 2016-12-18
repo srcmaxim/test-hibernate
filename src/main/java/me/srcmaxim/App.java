8b95d5dacc456078f276fc4ff8397da14c7049dc
srcmaxim <srcmaxim@gmail.com>
2016-12-19 00:51:48 +0200

    Add example of second level cacheable query

--- src/main/java/me/srcmaxim/App.java
+++ src/main/java/me/srcmaxim/App.java
@@ -2,31 +2,22 @@ package me.srcmaxim;
 
 import me.srcmaxim.dao.User;
 
+import java.util.stream.IntStream;
+
 import static me.srcmaxim.util.HibernateUtil.*;
 
 public class App {
 
     public static void main(String[] args) {
         openInASession(session -> {
-            session.save(new User("User 1"));
+            IntStream.rangeClosed(1, 10).forEach(i -> session.save(new User("User " + i)));
         });
 
-        openInASession(session -> {
-            // new select because of closed session
-            // Hibernate: select user0_.USER_ID as USER1_0_0_, user0_.USERNAME as USERNAME0_0_ from USER_DETAILS user0_ where user0_.USER_ID=?
-            User userA = (User) session.get(User.class, 1);
-            userA.setUsername("User 1 with changed name");
-            // don't do select and get it from the cache
-            User userB = (User) session.get(User.class, 1);
-            // must update db
-            // Hibernate: update USER_DETAILS set USERNAME=? where USER_ID=?
-        });
+        openInASession(session -> session.createCriteria(User.class).setCacheable(true)
+                .list().stream().forEach(System.out::println));
 
-        openInASession(session -> {
-            // new select because of closed session
-            // Hibernate: select user0_.USER_ID as USER1_0_0_, user0_.USERNAME as USERNAME0_0_ from USER_DETAILS user0_ where user0_.USER_ID=?
-            User userA = (User) session.get(User.class, 1);
-        });
+        openInASession(session -> session.createCriteria(User.class).setCacheable(true)
+                .list().stream().forEach(System.out::println));
     }
 
 }
