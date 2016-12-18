6bedf1aa38f660da83179242b6942a8afd0c379d
srcmaxim <srcmaxim@gmail.com>
2016-12-18 13:26:42 +0200

    Add example of pagination HQL

--- src/main/java/me/srcmaxim/App.java
+++ src/main/java/me/srcmaxim/App.java
@@ -1,6 +1,7 @@
 package me.srcmaxim;
 
 import me.srcmaxim.dao.User;
+import org.hibernate.Query;
 
 import java.util.List;
 import java.util.stream.IntStream;
@@ -9,11 +10,17 @@ import static me.srcmaxim.util.HibernateUtil.*;
 public class App {
 
 	public static void main(String[] args) {
-		IntStream.rangeClosed(1,10).mapToObj(i -> new User("User " + i))
+		IntStream.rangeClosed(1,12).mapToObj(i -> new User("User " + i))
 				.forEach(user -> openInASession(session -> session.save(user)));
 
-		openInASession(session -> ((List<User>)session.createQuery("from User where userId > 5").list())
-				.stream().map(user -> user.getUsername()).forEach(System.out::println));
+		openInASession(session -> {
+			Query query = session.createQuery("from User");
+			query.setMaxResults(3);
+			query.setFirstResult(0);
+			((List<User>) query.list()).stream().map(user -> user.getUsername()).forEach(System.out::println);
+			query.setFirstResult(4);
+			((List<User>) query.list()).stream().map(user -> user.getUsername()).forEach(System.out::println);
+		});
 	}
 
 }
